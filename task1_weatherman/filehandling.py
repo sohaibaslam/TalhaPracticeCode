86ddbc1f15b7f16ec86fbc1cb777d54ca1d531dc
Talha Yousuf Sameja <talha.samej@gmail.com>
2024-03-28 00:00:05 +0500

    used datetime library in has_year_month_in_pkt function

--- task1_weatherman/filehandling.py
+++ task1_weatherman/filehandling.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from datetime import datetime
 
 class FileReader:
     
@@ -17,17 +18,16 @@ class FileReader:
         return file_name_with_year_month
 
 
-    def has_year_month_in_pkt(self, file_path,target_year, target_month):
+    def has_year_month_in_pkt(self, file_path, target_year, target_month):
         with open(file_path, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                pkt_date_parts = row.get('PKT', '').split('-')
-                if len(pkt_date_parts) == 3:
-                    year, month, date = pkt_date_parts
-                    if year == str(target_year) and month == str(target_month):
+                pkt_date = row.get('PKT', '')
+                if pkt_date:
+                    pkt_date = datetime.strptime(pkt_date, '%Y-%m-%d')
+                    if pkt_date.year == target_year and pkt_date.month == target_month:
                         return True
-        
-            return False
+        return False
 
     def get_file_data (self,file_name):
         file_path = os.path.join(self.folder_path,file_name)
